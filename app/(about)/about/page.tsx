d05a1e1c0347cf38d0db5c05901fa03781b9d05b
unknown <kimkr88@icloud.com>
2024-11-14 13:37:55 +0900

    feat: Complete About page

---
+++ app/(about)/about/page.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold mb-12">About us</h1>
+      <p className="text-xl whitespace-pre-wrap leading-10">
+        Welcome to the official explorer for the New York Times Best Seller list
+        explorer.
+        <br />
+        We hope you enjoy your stay!
+      </p>
+    </>
+  );
+}
