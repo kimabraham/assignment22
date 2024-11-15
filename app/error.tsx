ba77c6882d7133ea34e589c6ca37f091f3facc9d
unknown <kimkr88@icloud.com>
2024-11-15 11:53:53 +0900

    feat: add error page & not-found page

---
+++ app/error.tsx
@@ -0,0 +1,17 @@
+"use client"; // Error boundaries must be Client Components
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <p>{error.message}</p>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-24` lg:px-6">
+      <div className="mx-auto max-w-screen-sm text-center">
+        <h1 className="mb-12 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600">
+          404
+        </h1>
+        <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">
+          Something&apos;s missing.
+        </p>
+        <p className="mb-16 text-lg font-light text-gray-500">
+          Sorry, we can&apos;t find that page. You&apos;ll find lots to explore
+          on the home page.
+        </p>
+        <Link href="/" className="btn btn-primary">
+          Back to Homepage
+        </Link>
+      </div>
+    </div>
+  );
+}
